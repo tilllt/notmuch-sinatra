3a22bf757f58056a811fa5a4a85d70aedf6c7706
hans meiser <tilllt@yahoo.com>
2016-02-24 13:58:48 +0100

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,41 @@
+#
+# notmuch-sinatra
+# A Ruby / Sinatra / AngularJS GUI for notmuch
+# v0.0.1
+#
+
+require 'sinatra'
+require 'sinatra/assetpack'
+
+get '/' do
+	File.read "#{Dir.pwd}/index.html"
+end
+
+assets do
+	serve '/bower_components', from: 'bower_components'
+end
+
+get '/notmuch/search' do
+	content_type :json
+	system('/home/till/notmuch search --format=\"json\"'+ params)
+end
+
+get '/notmuch/show' do
+	content_type :json
+	system('/home/till/notmuch show --format=\"json\"'+ params)
+end
+
+get '/notmuch/address' do
+	content_type :json
+	system('/home/till/notmuch address --format=\"json\"'+ params)
+end
+
+get '/notmuch/count' do
+	content_type :json
+	system('/home/till/notmuch count --format=\"json\"'+ params)
+end
+
+not_found do
+	content_type :json
+	halt 404, { error: 'URL not found' }.to_json
+end
